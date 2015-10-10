1c105bb130e766c2056a27199053f3a535c2d367
rastapanda <me@jspenc.com>
2015-10-10 13:08:03 +0100

    added missing project

---
+++ introduction-module-1-koan-1/Program.fs
@@ -0,0 +1,21 @@
+﻿// Learn more about F# at http://fsharp.org
+// See the 'F# Tutorial' project for more help.
+open System
+
+[<EntryPoint>]
+let main argv = 
+    printfn "%A" argv
+
+
+    let mutable a = 100
+    let b = 1000
+
+    a <- 101
+
+    let f x y = x + y / 10
+
+    printfn "%A" (f a b)
+
+    Console.ReadKey()
+
+    0 // return an integer exit code
