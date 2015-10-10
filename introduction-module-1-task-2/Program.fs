ceabf05a8f6eb506af085f572acfd8912b1b4080
rastapanda <me@jspenc.com>
2015-10-10 11:33:14 +0100

    mod 1 task 2

---
+++ introduction-module-1-task-2/Program.fs
@@ -0,0 +1,20 @@
+﻿// Learn more about F# at http://fsharp.org
+// See the 'F# Tutorial' project for more help.
+open System
+
+let areaOfAHexagon t =
+    (3.0 * Math.Sqrt(3.0) / 2.0) * t * t
+    
+[<EntryPoint>]
+let main argv = 
+    printfn "%A" argv
+    Console.WriteLine("enter the size of the hexagon")
+    let size = Console.ReadLine()
+    let size = float size
+    let computedArea = areaOfAHexagon size
+
+    Console.WriteLine("the area of the hexagon is: " + (string computedArea) )
+
+    Console.ReadKey()
+
+    0 // return an integer exit code
