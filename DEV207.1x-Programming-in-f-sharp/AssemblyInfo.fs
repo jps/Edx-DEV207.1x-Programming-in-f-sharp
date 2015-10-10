02f96d1edcaddb575c5b277ec16cbb51d2986560
rastapanda <me@jspenc.com>
2015-10-10 10:24:03 +0100

    init

---
+++ DEV207.1x-Programming-in-f-sharp/AssemblyInfo.fs
@@ -0,0 +1,41 @@
+﻿namespace DEV207._1x_Programming_in_f_sharp.AssemblyInfo
+
+open System.Reflection
+open System.Runtime.CompilerServices
+open System.Runtime.InteropServices
+
+// General Information about an assembly is controlled through the following 
+// set of attributes. Change these attribute values to modify the information
+// associated with an assembly.
+[<assembly: AssemblyTitle("DEV207.1x-Programming-in-f-sharp")>]
+[<assembly: AssemblyDescription("")>]
+[<assembly: AssemblyConfiguration("")>]
+[<assembly: AssemblyCompany("")>]
+[<assembly: AssemblyProduct("DEV207.1x-Programming-in-f-sharp")>]
+[<assembly: AssemblyCopyright("Copyright ©  2015")>]
+[<assembly: AssemblyTrademark("")>]
+[<assembly: AssemblyCulture("")>]
+
+// Setting ComVisible to false makes the types in this assembly not visible 
+// to COM components.  If you need to access a type in this assembly from 
+// COM, set the ComVisible attribute to true on that type.
+[<assembly: ComVisible(false)>]
+
+// The following GUID is for the ID of the typelib if this project is exposed to COM
+[<assembly: Guid("4c46704b-ea3a-4d1d-9017-1eebf043ce8a")>]
+
+// Version information for an assembly consists of the following four values:
+// 
+//       Major Version
+//       Minor Version 
+//       Build Number
+//       Revision
+// 
+// You can specify all the values or you can default the Build and Revision Numbers 
+// by using the '*' as shown below:
+// [<assembly: AssemblyVersion("1.0.*")>]
+[<assembly: AssemblyVersion("1.0.0.0")>]
+[<assembly: AssemblyFileVersion("1.0.0.0")>]
+
+do
+    ()
---
+++ DEV207.1x-Programming-in-f-sharp/Program.fs
@@ -0,0 +1,21 @@
+﻿// Learn more about F# at http://fsharp.org
+// See the 'F# Tutorial' project for more help.
+open System
+
+[<EntryPoint>]
+let main argv = 
+    printfn "%A" argv
+    Console.WriteLine("Please enter how far you have travelled:")
+    let distance = Console.ReadLine()
+    let distance = float distance
+    Console.WriteLine("Please enter how much fuel you have used:")
+    let fuel = Console.ReadLine()
+    let fuel = float fuel
+    
+    let distancePerUnitOfFuel = distance / fuel; 
+
+    Console.WriteLine("Your car does a distance of " + (string distancePerUnitOfFuel) + "units per unit of fuel" )
+
+    Console.ReadKey()
+
+    0 // return an integer exit code
