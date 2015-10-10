9e64da687558d34955722c7c798da248b9730cc3
rastapanda <me@jspenc.com>
2015-10-10 12:23:38 +0100

    module 1 problem 1

---
+++ introduction-module-1-problem-1/Program.fs
@@ -0,0 +1,21 @@
+﻿open System
+
+let volumeOfCylinder radius height =
+    (Math.PI * Math.Pow(radius, 2.0)) * height
+
+[<EntryPoint>]
+let main argv = 
+
+    printfn "Enter the HEIGHT of the cylinder:"
+    let height = Console.ReadLine() |> Microsoft.FSharp.Core.float.Parse
+
+    printfn "Enter the RADIUS of the cylinder:"
+    let radius = Console.ReadLine() |> Microsoft.FSharp.Core.float.Parse
+
+    let computedVolume = volumeOfCylinder radius height
+
+    printfn " The computed volume of the cylinder is: %f units" computedVolume
+
+    Console.ReadKey()
+
+    0 // return an integer exit code
