91bae6daada0851bfc53f95fad7bcc8d7f300f5a
rastapanda <me@jspenc.com>
2015-10-11 16:20:45 +0100

    mod 2 lab 2

--- conditionals-recursion/Program.fs
+++ conditionals-recursion/Program.fs
@@ -1,7 +1,23 @@
 ﻿// Learn more about F# at http://fsharp.org
 // See the 'F# Tutorial' project for more help.
 
+open System
+
+let rec calcFib x =
+    if x <= 2 then 1
+    else calcFib(x-1) + calcFib(x-2)
+
 [<EntryPoint>]
 let main argv = 
-    printfn "%A" argv
+    printfn "Enter a value to calulate the fib:"
+    
+    let value = Console.ReadLine()
+    let value = int value
+
+    let result = calcFib(value)
+
+    printfn "result is: %i" result
+
+    Console.ReadKey()
+
     0 // return an integer exit code
