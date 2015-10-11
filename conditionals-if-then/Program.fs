0fe055f7ef441f55d87a2565dc988d7578b55598
rastapanda <me@jspenc.com>
2015-10-11 14:26:32 +0100

    started module 2 - conditionals

---
+++ conditionals-if-then/Program.fs
@@ -0,0 +1,26 @@
+﻿// Learn more about F# at http://fsharp.org
+// See the 'F# Tutorial' project for more help.
+
+open System
+
+
+let isPositiveEven v = 
+    if v < 0 then
+        false
+    elif (v % 2) = 0 then
+        true
+    else
+        false
+
+[<EntryPoint>]
+let main argv = 
+    
+    let check = Console.ReadLine()
+    let check = int check
+    
+    let result = isPositiveEven check
+
+    printfn "%b" result
+
+    Console.ReadKey()
+    0 // return an integer exit code
