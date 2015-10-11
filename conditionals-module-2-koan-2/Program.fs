41c3911d2f9d6b530691eb662b091607d0dc4555
rastapanda <me@jspenc.com>
2015-10-11 16:58:43 +0100

    koan 2

---
+++ conditionals-module-2-koan-2/Program.fs
@@ -0,0 +1,24 @@
+﻿// Learn more about F# at http://fsharp.org
+// See the 'F# Tutorial' project for more help.
+
+open System
+
+[<EntryPoint>]
+let main argv = 
+    
+    let input = Console.ReadLine()
+    let input = int input
+
+    let rec dosomethingrandom x =
+        if x = 0 then 
+            1
+        else 
+            dosomethingrandom (x - 1) * x
+
+    let result = dosomethingrandom(input)
+
+    printfn "%A" result
+
+    Console.ReadKey()
+
+    0 // return an integer exit code
