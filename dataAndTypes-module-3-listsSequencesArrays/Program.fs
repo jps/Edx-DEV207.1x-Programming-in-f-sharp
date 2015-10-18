c4532680f1ad9cbc26870c63ba47314aa382125e
rastapanda <me@jspenc.com>
2015-10-18 13:49:27 +0100

    list sequences and arrays added.

---
+++ dataAndTypes-module-3-listsSequencesArrays/Program.fs
@@ -0,0 +1,38 @@
+﻿// Learn more about F# at http://fsharp.org
+// See the 'F# Tutorial' project for more help.
+open System; 
+
+
+[<EntryPoint>]
+let main argv = 
+
+    //Arrays
+    let arrayA = [|1;2;3;4|];
+    printfn "%A" arrayA
+
+    let arrayB = [|for i in 1..10 -> i * i|];
+    printfn "%A" arrayB
+
+    //Lists
+    let listA = [1;2;3;4]
+    printfn "%A" listA
+
+    let listB = [ for i in 5 .. 10 -> i];
+    printfn "%A" listB
+
+    let addItemToList = 5 :: listA 
+    printfn "%A" addItemToList
+
+    let concatLists = listA @ listB
+    printfn "%A" concatLists
+
+    //Sequences
+    let sequenceA = seq { 0 .. 2 .. 10 }
+    printfn "%A" sequenceA 
+
+    let sequenceB = seq { for i in 1 .. 10 do yield i * i }
+    printfn "%A" sequenceB
+
+    Console.ReadKey()
+
+    0 // return an integer exit code
