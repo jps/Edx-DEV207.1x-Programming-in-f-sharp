2fc048918148dc934213e6166032abc7d41115ab
rastapanda <me@jspenc.com>
2015-10-18 17:03:43 +0100

    tuples and records

---
+++ dataAndTypes-module-3-tuples/AssemblyInfo.fs
@@ -0,0 +1,41 @@
+﻿namespace dataAndTypes_module_3_tuples.AssemblyInfo
+
+open System.Reflection
+open System.Runtime.CompilerServices
+open System.Runtime.InteropServices
+
+// General Information about an assembly is controlled through the following 
+// set of attributes. Change these attribute values to modify the information
+// associated with an assembly.
+[<assembly: AssemblyTitle("dataAndTypes-module-3-tuples")>]
+[<assembly: AssemblyDescription("")>]
+[<assembly: AssemblyConfiguration("")>]
+[<assembly: AssemblyCompany("")>]
+[<assembly: AssemblyProduct("dataAndTypes-module-3-tuples")>]
+[<assembly: AssemblyCopyright("Copyright ©  2015")>]
+[<assembly: AssemblyTrademark("")>]
+[<assembly: AssemblyCulture("")>]
+
+// Setting ComVisible to false makes the types in this assembly not visible 
+// to COM components.  If you need to access a type in this assembly from 
+// COM, set the ComVisible attribute to true on that type.
+[<assembly: ComVisible(false)>]
+
+// The following GUID is for the ID of the typelib if this project is exposed to COM
+[<assembly: Guid("bcf5fae8-af26-438f-8326-367c5442f8a2")>]
+
+// Version information for an assembly consists of the following four values:
+// 
+//       Major Version
+//       Minor Version 
+//       Build Number
+//       Revision
+// 
+// You can specify all the values or you can default the Build and Revision Numbers 
+// by using the '*' as shown below:
+// [<assembly: AssemblyVersion("1.0.*")>]
+[<assembly: AssemblyVersion("1.0.0.0")>]
+[<assembly: AssemblyFileVersion("1.0.0.0")>]
+
+do
+    ()
---
+++ dataAndTypes-module-3-tuples/Program.fs
@@ -0,0 +1,29 @@
+﻿// Learn more about F# at http://fsharp.org
+// See the 'F# Tutorial' project for more help.
+
+
+type person =
+    {
+        name: string;
+        mutable age : int
+    }
+
+[<EntryPoint>]
+let main argv = 
+    printfn "%A" argv
+    
+    //Create
+    let animal1 = ("Jack", "Dog", 1, 1229292)    
+    let animal2 = ("Rex", "Dinosaur", 12, 1229293)
+    let animal3 = ("Tom", "Cat", 7, 1229294)
+    //Decompose
+    let name, animaltype, age, id = animal1
+    
+
+    //Types
+    let dave = { name = "Dave Russell"; age = 35}
+
+    let myname = dave.name;
+
+
+    0 // return an integer exit code
