55a0bb97051d133ab838783be2f3ca837df1e6d0
rastapanda <me@jspenc.com>
2015-10-19 20:46:26 +0100

    module 3 koan module 3 task 1

---
+++ dataAndTypes-module-3-koan-3/Program.fs
@@ -0,0 +1,24 @@
+﻿// Learn more about F# at http://fsharp.org
+// See the 'F# Tutorial' project for more help.
+
+open System;
+
+[<EntryPoint>]
+let main argv = 
+    
+
+    let arr = [1; 2; 3; 4; 5; 6; 7; 8; 9]
+    let l = arr.Length - 1
+    let isEven x = 
+        x % 2 = 0
+
+    let out = seq { for i in 1 .. l do
+                if (isEven (arr.Item i)) then yield arr.[i] }
+
+    let newout =  [0] @ List.ofSeq(out)
+
+    printfn "%A" newout
+
+    Console.ReadKey()
+
+    0 // return an integer exit code
---
+++ dataAndTypes-module-3-task-1/Program.fs
@@ -0,0 +1,73 @@
+﻿// Learn more about F# at http://fsharp.org
+// See the 'F# Tutorial' project for more help.
+open System
+
+
+type User = {
+    name : string;
+    age : int;
+    id : int;
+}
+
+let getUserId (name : string) age : int =
+    name.Length * age
+    
+let readUser() : User = 
+    printfn "Please enter user name:"
+    let inputedName = Console.ReadLine()
+    
+    printfn "Please enter user age:"
+    let inputedAge = Console.ReadLine()
+    let inputedAge = int inputedAge 
+    
+    { 
+        name = inputedName; 
+        age = inputedAge; 
+        id = (getUserId inputedName inputedAge)
+    }
+
+let rec enterAnother() =
+    printfn "add another user Y/N?"
+    let yesNo = Console.ReadLine()
+    let yesNo = yesNo.Trim().ToLower()
+
+    if yesNo = "y" then
+        true
+    elif yesNo = "n" then
+        false
+    else
+        printfn "Undexpected input"
+        enterAnother()
+
+let rec addUsers (users : List<User>) : List<User> =
+    let newUser = readUser()
+    let newUserList = newUser :: users 
+        
+    if enterAnother() then
+        addUsers newUserList
+    else
+        newUserList
+
+let printUser user =
+    printfn "name %s age %i id %i" user.name user.age user.id
+
+
+[<EntryPoint>]
+let main argv = 
+    printfn "%A" argv
+
+    //Main user
+    let mainUser = readUser()
+    //Add other users
+    let otherUsers = addUsers List.empty<User>
+
+    printfn "Main user:"
+    printUser mainUser
+    printfn "Matched users:"
+    (List.filter(fun x -> x.id = mainUser.id) otherUsers) 
+        |> List.iter printUser
+        
+    Console.ReadKey()
+    0 // return an integer exit code
+
+
