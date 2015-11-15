acb7b2b87ee32c227d644245194ac17f19cb752c
rastapanda <me@jspenc.com>
2015-11-15 16:18:04 +0000

    koan 4

---
+++ patternMatching-module-4-koan-4/Program.fs
@@ -0,0 +1,26 @@
+﻿open System
+
+type Adapter =
+    { IP : string
+      MAC : string
+      FriendlyName : string
+      ID : int }
+
+let (|IsMatchByName|_|) (input : Adapter) (name: string) =
+    match input with
+    | {FriendlyName = friendlyName} when friendlyName = name -> Some((input.ID,input.IP))
+    | _ -> None
+ 
+let checkmatch (record: Adapter) (name : string) =
+    match name with 
+    | IsMatchByName record name -> ""
+    | _ -> "No match"
+
+[<EntryPoint>]
+let main argv = 
+    let testRecord = {IP = "10.1.1.1"; MAC = "FF:FF:FF:FF:FF:FF"; FriendlyName = "ServerFailure";ID = 0} 
+    let result = checkmatch testRecord "ServerFailure" 
+       
+    Console.WriteLine(result)
+    Console.ReadKey() |> ignore
+    0 // return an integer exit code
