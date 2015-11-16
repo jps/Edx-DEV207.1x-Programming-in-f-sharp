3bdf817266983c509531d2cfa1db1af8d21995fa
rastapanda <me@jspenc.com>
2015-11-16 19:38:14 +0000

    project rename

--- patternMatching-module-4-problem-4/Program.fs
+++ patternMatching-module-4-problem-4/Program.fs
@@ -1,58 +1,26 @@
-﻿open System; 
-open System.IO;
+﻿open System
 
-type Point = { X: int; Y: int; Name: string }
+type Adapter =
+    { IP : string
+      MAC : string
+      FriendlyName : string
+      ID : int }
 
-//define pattern
-let (|Quadrant1|Quadrant2|Quadrant3|Quadrant4|Origin|Undefined|) input = 
+let (|IsMatchByName|_|) (input : Adapter) (name: string) =
     match input with
-        | {Point.X = x; Point.Y = y; Point.Name = name;} when x > 0 && y > 0 -> Quadrant1 //Point.X = 0 & Point.Y > 0
-        | {Point.X = x; Point.Y = y; Point.Name = name;} when x < 0 && y > 0 -> Quadrant2
-        | {Point.X = x; Point.Y = y; Point.Name = name;} when x < 0 && y < 0 -> Quadrant3
-        | {Point.X = x; Point.Y = y; Point.Name = name;} when x > 0 && y < 0 -> Quadrant4
-        | {Point.X = x; Point.Y = y; Point.Name = name;} when x = 0 && y = 0 -> Origin
-        | _ -> Undefined
-    
-    
+    | {FriendlyName = friendlyName} when friendlyName = name -> Some()
+    | _ -> None
+ 
+let checkmatch (record: Adapter) (name : string) =
+    match name with 
+    | IsMatchByName record -> "Match"
+    | _ -> "No match"
 
 [<EntryPoint>]
 let main argv = 
-
-    let getFile =
-        Console.Write("Enter the full path to the name of the input file: ")
-        Console.ReadLine()
-
-    try
-        use input =
-            new StreamReader(match argv.Length with
-                            | 0 -> getFile
-                            | _ -> argv.[0])
-        let entities = 
-            [ while not input.EndOfStream do
-                let raw = input.ReadLine()
-                let values = raw.Split(',')
-                yield { X = int values.[0]
-                        Y = int values.[1]
-                        Name = values.[2] } ]
-        let errornames =
-            seq {
-                for p in entities ->
-                    match p with
-                    | Undefined -> Some(p.Name)
-                    | _ -> None
-            }
-        Console.WriteLine("The undefined entries are: ")
-        for e in errornames do
-            match e with
-            | Some i -> Console.WriteLine(i)
-            | None -> ()
-        Console.ReadKey() |> ignore
-        0
-    with
-    | :? System.IO.FileNotFoundException ->
-        Console.Write("File Not Found. Press a key to exit")
-        Console.ReadKey() |> ignore
-        -1
-    | _ ->
-        Console.Write("Something else happened")
-        -1 
+    let testRecord = {IP = "10.1.1.1"; MAC = "FF:FF:FF:FF:FF:FF"; FriendlyName = "ServerFailure";ID = 0} 
+    let result = checkmatch testRecord "ServerFailure" 
+       
+    Console.WriteLine(result)
+    Console.ReadKey() |> ignore
+    0 // return an integer exit code
